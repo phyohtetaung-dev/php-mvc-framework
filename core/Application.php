86bc63b2e5c544da96b4578173211fe4b1e57e72
phyohtetaung-dev <phyohtetaung.dev.29@gmail.com>
2022-06-28 13:19:08 +0630

    Implement views & layouts

--- core/Application.php
+++ core/Application.php
@@ -4,16 +4,22 @@ namespace app\core;
 
 class Application
 {
+    public static $ROOT_DIR;
     public $request;
+    public $response;
     public $router;
+    public static $app;
 
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run() {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
---
+++ views/contact.php
@@ -0,0 +1,26 @@
+<div class="card">
+    <div class="card-header">Contact Us</div>
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="mb-3 row">
+                <label>Subject</label>
+                <div class="col-sm-10">
+                    <input type="text" name="subject" class="form-control">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label>Email</label>
+                <div class="col-sm-10">
+                    <input type="email" name="email" class="form-control">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label>Body</label>
+                <div class="col-sm-10">
+                    <textarea name="body" class="form-control"></textarea>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
