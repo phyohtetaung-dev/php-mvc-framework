92ddc88e5db5960670b8e2ffcfc590a832aac825
phyohtetaung-dev <phyohtetaung.dev.29@gmail.com>
2022-06-29 19:02:04 +0630

    Add NOTES.md and fixed max validation condition

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+# Essential Functions
+
+1. include vs require
+2. include_once vs require_once
+3. ob_start and ob_get_clean
+4. call_user_func
+5. filter_input
+6. filter_var
+
+# Others
--- core/Model.php
+++ core/Model.php
@@ -41,7 +41,7 @@ abstract class Model
                 if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
                     $this->addError($attribute, self::RULE_MIN, $rule);
                 }
-                if ($ruleName === self::RULE_MAX && strlen($value) < $rule['max']) {
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
                     $this->addError($attribute, self::RULE_MAX, $rule);
                 }
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
